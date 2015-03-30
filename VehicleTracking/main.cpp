e5019437feaeda77b3414cdff3e1a2b2a3363262
Quan Hua <quanhua92@gmail.com>
2015-03-30 16:00:38 +0700

    VT - Setup VehicleTracking Project

---
+++ VehicleTracking/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Vehicle Detection & Tracking!" << endl;
+
+    // Car detection for initialization
+
+    // for each frame
+        // Car tracking
+
+
+
+    return 0;
+}
