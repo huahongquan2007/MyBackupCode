ca851ee051b3a3fac1e01357d487843a0fb97a15
Quan Hua <quanhua92@gmail.com>
2015-07-23 03:17:38 +0700

    add opencv_java

--- OpenCV_Blueprints_04_panorama_submit/Panorama/app/src/main/java/com/example/panorama/MainActivity.java
+++ OpenCV_Blueprints_04_panorama_submit/Panorama/app/src/main/java/com/example/panorama/MainActivity.java
@@ -12,7 +12,6 @@ import android.graphics.PorterDuff;
 import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -23,7 +22,6 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
-
 
 public class MainActivity extends ActionBarActivity {
 
