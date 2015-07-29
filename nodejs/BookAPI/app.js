9a70c1ba99d27f8cad19eb8bb7e1748504e95501
Quan Hua <quanhua92@gmail.com>
2015-07-29 22:51:17 +0700

    Update code

---
+++ nodejs/BookAPI/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
+
+app.get('/', function(req, res){
+   res.send('welcome to my API');
+});
+
+app.listen(port, function(){
+    console.log('Running on PORT: ' + port);
+});
