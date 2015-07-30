65a9dbfef5e62a85a458502f72581e2068dc733e
Quan Hua <quanhua92@gmail.com>
2015-07-30 23:17:00 +0700

    move post and get to BookController

--- nodejs/BookAPI/Routes/bookRoutes.js
+++ nodejs/BookAPI/Routes/bookRoutes.js
@@ -3,25 +3,11 @@ var express = require('express');
 var routes = function(Book){
     var bookRouter = express.Router();
 
-    bookRouter.route('/')
-        .post(function(req, res){
-            var book = new Book(req.body);
-            book.save();
+    var bookController = require('../Controllers/bookController')(Book);
 
-            res.status(201).send(book);
-        })
-        .get(function(req, res){
-            var query = {};
-            if(req.query.genre){
-                query.genre = req.query.genre;
-            }
-            Book.find(query, function (err, books) {
-                if(err)
-                    res.status(500).send(err);
-                else
-                    res.json(books);
-            });
-        });
+    bookRouter.route('/')
+        .post(bookController.post)
+        .get(bookController.get);
     // Middle Ware
     bookRouter.use('/:bookId', function(req, res, next){
         Book.findById(req.params.bookId, function (err, book) {
