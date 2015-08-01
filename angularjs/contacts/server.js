b2f8b3615c4a70381b235608de95613b9a52c92a
Quan Hua <quanhua92@gmail.com>
2015-08-01 09:32:17 +0700

    basic rest api

--- angularjs/contacts/server.js
+++ angularjs/contacts/server.js
@@ -2,9 +2,11 @@
  * Created by quanhua92 on 8/1/15.
  */
 var express = require('express'),
+    api = require('./api'),
     app = express();
 
 app.use(express.static('./public'))
+    .use('/api', api)
     .get('*', function(req, res){
        res.sendfile('public/main.html');
     })
