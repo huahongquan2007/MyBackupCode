f0394dfad9640fbe2d20222c0054bc249e2f7621
Quan Hua <quanhua92@gmail.com>
2015-08-08 16:58:30 +0700

    basic form

--- angularjs/ng-book/01-basic/app.js
+++ angularjs/ng-book/01-basic/app.js
@@ -3,12 +3,7 @@
  */
 angular.module("myApp", [])
     .controller("MyController", function ($scope, $timeout) {
-        $scope.clock = {};
-        var updateClock = function () {
-            $scope.clock.now = new Date();
-            $timeout(function(){
-                updateClock();
-            }, 1000);
-        };
-        updateClock();
+        $scope.person = {
+            name: "Ari Lerner"
+        }
     });
