e3db868dd63e2ba1f82ad1dd094faf7acd687945
Quan Hua <quanhua92@gmail.com>
2015-08-11 23:30:19 +0700

    basic validation in signup form

---
+++ angularjs/ng-book/02-sign-up-form/app.js
@@ -0,0 +1,23 @@
+angular.module('myApp', [])
+    .directive('ensureUnique', ['$http', function($http) {
+        return {
+            required: 'ngModel',
+            link: function(scope, ele, attrs, c) {
+                scope.$watch(attrs.ngModel, function(n) {
+                    if (!n) return;
+                    $http({
+                        method: 'POST',
+                        url: '/api/check/' + attrs.ensureUnique,
+                         data: {
+                             field: attrs.ensureUnique,
+                             value: scope.ngModel
+                         }
+                    }).success(function(data) {
+                        c.$setValidity('unique', data.isUnique);
+                    }).error(function(data) {
+                        c.$setValidity('unique', false);
+                    })
+                })
+            }
+        }
+    }]);
