aec2a42d9f2c39c471635b9e056df7727b1c425b
Quan Hua <quanhua92@gmail.com>
2015-12-01 13:54:21 +0700

    Add map2 to my location

--- Coursera/object-oriented-java/course01/UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ Coursera/object-oriented-java/course01/UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -65,7 +65,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 30, 50, 350, 500, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
@@ -76,7 +76,12 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 420, 50, 350, 500, provider);
 
+		// The next line zooms in and centers the map at 
+	    // 32.9 (latitude) and -117.2 (longitude)
+	    map2.zoomAndPanTo(zoomLevel, new Location(10.812907, 106.700473));
+	    MapUtils.createDefaultEventDispatcher(this, map2);
 	}
 
 	/** Draw the Applet window.  */
@@ -84,6 +89,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
